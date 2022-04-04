d15634fe8613f65da99b5dfd6dac16e108140e75
shariful islam <sharifulb07@gmail.com>
2022-04-04 08:03:21 -0700

    javascript conditional practice

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
-let ladder={
-    step:0,
-    up(){
-        this.step++;
-        return this;
+// let login=prompt("Input your Designation?",'');
 
-    },
-    down(){
-        this.step--;
-        return this;
-    },
+// let message;
+// (login=="Employee")? message="you are employee":(login=="Director")? message="you are Director":(login=="")? message="No login":message="oh no";
 
-    showStep:function(){
-       console.log(this.step);
-       return this;
-    }
-}
+// alert(message);
 
-ladder.up().up().showStep();
+
+
+
+// ternary conditional
+// let x=10;
+// let y=(x==10)?"this is correct":"this is not correct";
+// console.log(y);
+
+// let distance=10;
+
+// let nameOfStation=(distance<=5)?"you ar on madhobpur":(distance<=10)?"your are islampur":(distance==15)?"you are on dhaka":"You don't start your journey";
+
+// let a=prompt("Please input the value of a",'');
+// let b=prompt("Please input the value of b",'');
+
+// let result;
+// if(a+b<15){
+//     result="Below";
+// }else{
+//     result="Over";
+// }
+// console.log(result);
+
+
+
+// console.log(nameOfStation);
+
+// if("0"){
+//     alert("i am wrong");
+// }
+
+// let number=10;
+// if(number>0){
+//     alert("The value is greater than zero");
+// }else if(number==0){
+//     alert("The value is 0");
+// }else{
+//     alert("The value is less than zero");
+// }
+
+
+
+// let year=prompt('In which year you are born?','');
+
+// if(year<2015){
+//     alert("you was born too early");
+// }else if(year==2015) {
+//     alert("you was born on right time");
+// }else{
+//     alert("you are too late");
+// };
+
+
+
+// let accessAllowed;
+// let age=prompt('How old are you?','');
+
+// if(age>18){
+//     accessAllowed=true;
+// }else{
+//     accessAllowed=false;
+// };

b17a8c43d0af2ffee0b10ad6f4e994bb200746b2
shariful islam <sharifulb07@gmail.com>
2022-04-05 00:08:05 -0700

    if ... else statement and switch statement is being practised here opps!

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 
-//Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
 
-function pairwise(arr, arg) {
-    let pairIndices = [];
-    // Check every pair
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        // Exclude pairs that contain previously paired elements
-        if (arr[i] + arr[j] == arg
-            && !pairIndices.includes(i)
-            && !pairIndices.includes(j)) {
-          pairIndices.push(i, j);
-          break;
-        }
-      }
+    let a=2+2;
+    switch(a){
+        case 1:
+            console.log("This is number one ");
+            break;
+        case 3:
+            console.log("this is number threee");
+            break;
+        case 4:
+            console.log("this is number four");
+            break;
+        case 5:
+            console.log("this is number five");
+            break;
+        
     }
-  
-    return pairIndices.reduce((sum, curr, index) => sum + curr, 0);
-  }
-  
-  pairwise([1,4,2,3,0,5], 7);
+ 
+
+    // another switch statesment
+    let arg=prompt("Enter a value?");
+console.log(typeof arg);
+let num=Number(arg);
+switch(num){
+    case 0:
+    case 1:
+        alert('One or Zero');
+    case 2:
+        alert('Two');
+        break;
+    case 3:
+        alert ('Never executes!');
+               break;
+    default:
+        alert("An unknow value");
+
+}
+
+
+
+//  converting from switch statement to if... else statement
+let browser=prompt("Please Enter your Browser name here ");
+if(browser=="Edge"){
+    alert("You 've got the Edge!");
+}else if(browser=="Chrome" ||
+         browser=="FireFox" ||
+         browser=="Safari" ||
+         browser=="Opera"){
+    alert('Okay we support these browsers too');
+}else{
+    alert('We hope that this page looks ok!');
+}
+

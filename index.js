bb6b9bf94bdc06922febb76216a560c9c2af8a07
shariful islam <sharifulb07@gmail.com>
2022-04-06 07:31:49 -0700

    nodejs some core features practices like fs events and so

--- index.js
+++ index.js
@@ -1,208 +1,43 @@
-// let userName="Shariful Islam";
-
-// function showName(){
-//     let message="Hello "+userName;
-//     alert(message);
-//     console.log(userName);
-// }
-
-// showName();
-// showName();
+// const path=require('path');
+// const { posix } = require('path/posix');
+// const myPath="C:/Users/Adiyat/Desktop/backend/index.js";
+// console.log(path.dirname(myPath));
+// console.log(path.isAbsolute(myPath));
+// // console.log(path.posix)
+// console.log(path.normalize(path));
 
 
 
-// let userName="John";
+// var path=require('os');
+// console.log(path.homedir());
+// console.log(path.cpus());
+// console.log(path.freemem());
 
-// function showName(){
-//     userName="Bobita"; // chaned the outer variable
-//     let message="Hello "+userName;
-//     console.log(message);
+// const fs=require('fs');
+// fs.writeFileSync("myFile.txt","Hello Sharif your are doing well!");
+// fs.appendFileSync("myFile.txt","Hello Sharif how are you");
 
-// }
+// const data=fs.readFile('myFile.txt', (err,data)=>{
+//     console.log(data.toString());
+// });
+// // console.log(data.toString());
 
-// console.log(userName); // calling userName variable with the value John before function call
-// showName(); // function call
-// console.log(userName); // output would be Bobita
+// console.log('Hello')
 
+// const EventEmitter=require('events');
+// const School = require('./school');
+// // const { emit } = require('process');
+// const startPeriod = require('./school');
+// const emitter=new EventEmitter();
 
-// Declare and calling Function with two paramaters;
+// const startPeriod=require('./school');
+const School =require('./school');
+const school=new School();
 
-// function showMs(name, message){
-//     console.log(message+" "+name);
-// }
+school.on('bellRing',({period,text})=>{
+    console.log(`We need ${period} to be run here ${text}`);
+})
 
-// // Invoking the function 
+//raise an event 
 
-// showMs("Ashia", "How are you right now! ");
-// showMs("Adiyat", "What's up Baba");
-
-
-// calling a function with two parameter in which one intializes value passing
-
-// function useMessage(name, text=", Where are you"){
-//     console.log(name+text);
-// }
-
-// // invoking function calling here 
-
-// useMessage("Sharifa");
-// useMessage("masuma");
-
-// declaring function with another function
-
-// function anotherFunction(){
-//     return "What's man here to go ahead! ";
-// }
-// function myFunc(name, text=anotherFunction()){
-//     console.log(name+" "+text);
-// }
-
-
-// // invoke the function here
-
-// myFunc("shariful ");
-
-
-// function showMan(text){
-
-//     if(text==undefined){
-//         text="Empty text here"
-//     }
-//     console.log(text);
-// }
-
-// showMan();
-
-// nullish coalesing parameter within the function 
-
-// function showText(count){
-//     console.log(count??"Unknown");
-// }
-
-// showText(0);
-// showText(null);
-// showText(undefined);
-
-
-
-// user defined math function
-// function Sum(a,b){
-//     console.log(a+b);
-//     return a+b;
-// }
-// function Subtraction(a,b){
-//     console.log(a-b);
-//     return a-b;
-// }
-// function Multiply(a,b){
-//     console.log(a*b);
-//     return a*b;
-// }
-// function Modulus(a,b){
-//     console.log(a%b);
-//     return a%b;
-// }
-// function Division(a,b){
-//     console.log(a/b);
-//     return a/b;
-// }
-
-// Sum(17,89);
-// Subtraction(17,89);
-// Multiply(17,89);
-// Modulus(17,89);
-
-// Math.abs(Division(17,89)/1000);
-
-
-// function checkAge(age){
-//     if(age>=18){
-//         return true;
-//     }else{
-//         return confirm("Do you have any permission from your parents?");
-//     }
-// }
-
-
-// function checkAge(age){
-//     if(age>=18){
-//         return true;
-//     }else{
-//         return confirm("Do you have any permission from your parents?");
-//     }
-// }
-
-// let age=prompt("How old are you?", 18)
-
-// if(checkAge(age)){
-// alert("access Granted");
-// }else{
-//     alert("Access denied");
-// }
-
-
-// function doNothing(){
-//     return;
-// }
-
-// alert(doNothing()==undefined);
-
-
-
-// function showPrime(n){
-//     for(let i=2;i<n;i++){
-//         if(!isPrime(i))continue;
-//         alert(i);
-//     }
-// }
-
-
-
-// function isPrime(n){
-//     for(let j=2;j<n;j++){
-//         if(n%j==0) return false;
-//     }
-//     return true;
-// }
-
-
-// function ask(question,yes,no){
-//     if(confirm())yes()
-//     else no();
-// }
-// function showOk(){
-//     alert("You agreed");
-// }
-
-// function showCancel(){
-//     alert("you cancel the execution of the function ");
-// }
-
-// ask("Do you agree?", showOk, showCancel);
-
-
-// function ask(question,yes,no){
-//     if(confirm()) yes();
-//     else no();
-// }
-
-// ask(
-//     "I am shariful islam, Do you agree?",
-//     function(){alert("You agreed")},
-//     function(){alert("You cancel the execuation of the program ")}
-    
-//     )
-
-
-// function declaration
-
-// function sum(a,b,c){
-//     return (a+b+c);
-// }
-
-// // function expression
-// let submission=function sum(a,b,c){
-//     return (a+b+c);
-// }
-
-
+school.startPeriod();
